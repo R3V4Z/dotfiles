7bb28bb457fa56069923e24bd0744f95fc583216
Revaz Nakhutsrishvili <r@revaz.xyz>
2025-02-13 21:00:08 +0400

    Enable relativenumber option

--- nvim/init.lua
+++ nvim/init.lua
@@ -105,7 +105,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
